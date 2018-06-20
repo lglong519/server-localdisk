1bb61375438e3a679f96e4d321c49fe60837f682
lglong519 <769141564@qq.com>
2018-06-20 19:36:09 +0800

    console client ip

--- common/Middlewares.js
+++ common/Middlewares.js
@@ -2,7 +2,7 @@ const nconf = require('nconf');
 const info = require('./info');
 
 const redirect = (req, res, next) => {
-	info(req.method, decodeURI(req.url));
+	info(req.ip.replace(/[a-z:]/gi, ''), `\x1B[32m${req.method}\x1B[39m`, decodeURI(req.url));
 	if (req.url.startsWith(`${nconf.get('SOURCE')}/`)) {
 		if (req.url.endsWith('/')) {
 			req.url = req.url.slice(0, -1);
